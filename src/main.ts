9dd0e5c5c2eeb1d116621ae03bccb9920ff64d97
Maxime404 <galissairemaxime24@gmail.com>
2021-03-03 20:42:35 +0100

    Init discution with child

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,20 @@ const os = require("os");
 if (cluster.isMaster) {
     console.log('Hello from Matser !')
     const nbCpus = os.cpus().length
-    console.log(nbCpus)
+    const processChilds = {}
+
+    for(let i = 0; i < nbCpus; i++) {
+        const childrenId = (i + 1).toString().padStart(4, '0')
+        const child = cluster.fork({ childrenId })
+
+        processChilds[childrenId] = child
+
+        child.on('message', payload => {
+            console.log(`Message from child ${child.id}`)
+            console.log(payload)
+        })
+    }
+} else {
+    console.log(`[${process.env.childrenId} I'm here !]`)
+    process.send({ message: 'hello' })
 }
